226e4966a91ee5e26bcdda00a8141f8b78b1594d
katherinecykes <katherinecykes@gmail.com>
2014-01-20 23:31:38 -0500

    - Replaced the old movement system with Physics Movements.  - Added Roll on Command.  - Separated Roll and Turn for the ship.  - Added rings to fly through.  - Added Planet Collision Event (Restarts Level).  - Added Propulsion Force and commands to accelerate/reverse.  - Modified Ship Texture.  - Added Planetary Physics Materials.  - Added "Follow Rotation" where ship follows camera orientation.  - Added Illumination in thrusters and boosters when accelerating.  - Added better Skybox (Still needs improvements).

---
+++ Assets/Scripts/PlanetCollideScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCollideScript : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision Detected!");
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+            Application.LoadLevel(0);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Trigger Detected!");
+        if (col.gameObject.name == "Player")
+        {
+            Destroy(col.gameObject);
+            Application.LoadLevel(0);
+        }
+    }
+}
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -3,56 +3,122 @@ using System.Collections;
 
 public class ThirdPersonController : MonoBehaviour
 {
-    // Directional Variables
-    // These variables are used for ship rotation and camera control.
-    float mouseSensitivity;
+    // Movement Variables
+    // These variables dictate the maximum speed and acceleration of the ship.
+    public float shipMass;
 
-    // Force Vectors
-    // These Variables are used to determine the net forces acting upon the ship.
-    Vector3 GravityForce;
-    Vector3 PropulsionForce;
-    Vector3 NetForce;
+    public float maxPropulsionForce;
+    public float minPropulsionForce;
+    public float maxPropulsionAccel;
+    public float maxPropulsionVelocity;
+    public float curPropulsionVelocity;
+    public float propulsionDrag;
 
-    // Mass Scalar
-    float mass;
+    public float maxTorqueForce;
+    public float minTorqueForce;
+    public float maxTorqueAccel;
+    public float maxTorqueVelocity;
+    public float curTorqueVelocity;
+    public float torqueDrag;
 
-    // Acceleration Vector
-    Vector3 NetAcceleration;
+    // Component Variables
+    // These are variables used to access different components of the Game Object.
+    public Camera cam;
+    public Rigidbody body;
+    public ConstantForce force;
+    public UnityEngine.Light[] lights;
 
-    // Velocity Vector
-    Vector3 NetVelocity;
+    // Control Variables
+    // These are used to define controller and keyboard/mouse parameters.
+    public float mouseSensitivity;
+    public float buttonThreshold;
+    public float TorqueX;
+    public float TorqueY;
+    public float TorqueZ;
+    public float Propulsion;
+
+    // Orientation Variables
+    // These are used to control body orientation towards a desired rotation similar to the camera.
+    public float targetHorRotation;
+    public float targetVerRotation;
+
+    public Quaternion bodyRot;
+    public Quaternion camRot;
 
 	// Use this for initialization
 	void Start ()
     {
-        // Directional Variables
-        mouseSensitivity = 1;
+        // Movement Variables
+        shipMass = 1.0f;
 
-        // Force Vectors
-        GravityForce = Vector3.zero;
-        PropulsionForce = Vector3.zero;
-        NetForce = Vector3.zero;
+        maxPropulsionForce = 50.0f * shipMass;
+        minPropulsionForce = 5.0f;
+        maxPropulsionAccel = maxPropulsionForce / shipMass;
+        maxPropulsionVelocity = 100.0f;
+        curPropulsionVelocity = 0.0f;
+        propulsionDrag = maxPropulsionAccel / maxPropulsionVelocity;
 
-        // Mass Scalar
-        mass = 0.0f;
+        maxTorqueForce = 0.25f * shipMass;
+        maxTorqueAccel = maxTorqueForce / shipMass;
+        maxTorqueVelocity = 0.5f;
+        curTorqueVelocity = 0.0f;
+        torqueDrag = maxTorqueAccel / maxTorqueVelocity;
 
-        // Acceleration Vector
-        NetAcceleration = Vector3.zero;
+        // Component Variables
+        cam = GetComponentInChildren<Camera>();
+        body = GetComponent<Rigidbody>();
+        force = GetComponent<ConstantForce>();
+        lights = GetComponentsInChildren<Light>();
 
-        // Velocity Vector
-        NetVelocity = Vector3.zero;
+        // Control Variables
+        mouseSensitivity = 8.0f;
+        buttonThreshold = 0.05f;
+        TorqueX = 0;
+        TorqueY = 0;
+        TorqueZ = 0;
+        Propulsion = 0;
+
+        // Orientation Variables
+        targetHorRotation = 0.0f;
+        targetVerRotation = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        Rigidbody body = GetComponent<Rigidbody>();
-
         float horRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
         float verRotation = Input.GetAxis("Mouse Y") * mouseSensitivity;
-        float rolRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
-        body.transform.Rotate(-rolRotation, -verRotation, horRotation);
 
-        body.transform.Translate(20 * Time.deltaTime, 0, 0);
+        cam.transform.Rotate(-verRotation, horRotation, 0);
+        Quaternion camRot = cam.transform.rotation;
+
+        bodyRot = Quaternion.Slerp(body.transform.rotation, cam.transform.rotation, 4.0f * Time.deltaTime);
+
+        body.transform.rotation = bodyRot;
+        cam.transform.rotation = camRot;
 	}
+
+    // FixedUpdate is called once every 0.02 seconds.
+    void FixedUpdate ()
+    {
+        foreach (Light l in lights)
+        { l.intensity = 8 * (body.velocity.magnitude / maxPropulsionVelocity); }
+
+        if (Mathf.Abs(Input.GetAxis("Horizontal")) > buttonThreshold)
+        { TorqueZ = Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal")); }
+        else { TorqueZ = 0; }
+
+        if (Mathf.Abs(Input.GetAxis("Vertical")) > buttonThreshold)
+        { Propulsion = Input.GetAxis("Vertical") / Mathf.Abs(Input.GetAxis("Vertical")); }
+        else { Propulsion = 0; }
+
+        force.relativeForce = new Vector3(0, 0, ((maxPropulsionForce - minPropulsionForce) * Propulsion) + minPropulsionForce);
+        force.relativeTorque = new Vector3(0, 0, -(maxTorqueForce * TorqueZ));
+
+        body.drag = 2 * propulsionDrag / (propulsionDrag * Time.fixedDeltaTime + 1);
+        body.angularDrag = 6 * torqueDrag / (torqueDrag * Time.fixedDeltaTime + 1);
+
+        curPropulsionVelocity = body.velocity.magnitude;
+        curTorqueVelocity = body.angularVelocity.magnitude;
+    }
 }
